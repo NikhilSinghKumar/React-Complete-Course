2aaece1898131ed590e2299c36b90ed0ef8bbc6f
Nikhil Kumar <nikhilsk369@gmail.com>
2024-09-16 18:29:13 +0530

    Passing Children

--- 19 Mapping/src/App.jsx
+++ 19 Mapping/src/App.jsx
@@ -1,14 +1,16 @@
+import Container from "./componenets/Container";
 import ErrorMessage from "./componenets/ErrorMessage";
 import FoodItems from "./componenets/FoodItems";
+import "./App.css";
 
 function App() {
   let foodItems = ["Dal", "Roti", "Chawal", "Papad", "Sabji"];
   return (
-    <>
-      <h1>Daily Healthy Food</h1>
+    <Container>
+      <h1 className="food-heading">Daily Healthy Food</h1>
       <ErrorMessage items={foodItems} />
       <FoodItems items={foodItems} />
-    </>
+    </Container>
   );
 }
 
